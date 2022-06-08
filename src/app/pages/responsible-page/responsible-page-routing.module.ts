ae31ce0c94cf0f462208d60957b181eaa10e0fd7
lallalina <laurettalipari@gmail.com>
2022-06-08 19:51:28 +0200

    grafica

--- src/app/pages/responsible-page/responsible-page-routing.module.ts
+++ src/app/pages/responsible-page/responsible-page-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { ResponsiblePageComponent } from './responsible-page.component';
 
 const routes: Routes = [{ path: '', component: ResponsiblePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,SharedModule]
 })
 export class ResponsiblePageRoutingModule { }
